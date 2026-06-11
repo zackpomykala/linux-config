99e952d86f670d399ab205abe8d369413660e1bc
Pomykala, Zack <zack.pomykala@jhuapl.edu>
2026-06-11 11:49:43 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ZSH_DIR="$HOME/.oh-my-zsh"
+ZSH_CUSTOM="${ZSH_CUSTOM:-$ZSH_DIR/custom}"
+
+echo "Linking .zshrc..."
+ln -sf "$DOTFILES_DIR/zsh/.zshrc" "$HOME/.zshrc"
+
+echo "Checking Oh My Zsh..."
+if [ ! -d "$ZSH_DIR" ]; then
+  echo "Installing Oh My Zsh..."
+  RUNZSH=no CHSH=no KEEP_ZSHRC=yes \
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+mkdir -p "$ZSH_CUSTOM/plugins"
+
+install_plugin() {
+  local name="$1"
+  local repo="$2"
+  local dest="$ZSH_CUSTOM/plugins/$name"
+
+  if [ ! -d "$dest/.git" ]; then
+    echo "Installing plugin: $name"
+    git clone "$repo" "$dest"
+  else
+    echo "Plugin already installed: $name"
+  fi
+}
+
+install_plugin "zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
+install_plugin "zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+install_plugin "zsh-bat" "https://github.com/fdellwing/zsh-bat.git"
+
+echo
+echo "Done. Reload with:"
+echo "exec zsh"
